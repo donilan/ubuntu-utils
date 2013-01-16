39a6078eada38668d7a327b9b4d89d32f9efacff
Doni <donilan.qq@gmail.com>
2013-01-16 22:14:09 +0800

    Update install/ubuntu-init-install.sh

--- install/ubuntu-init-install.sh
+++ install/ubuntu-init-install.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-sudo apt-get install qemu-kvm kvm emacs git svn openjdk-6-jdk curl chromium-browser mysql-server nginx -y
+sudo apt-get install qemu-kvm kvm emacs git subversion openjdk-6-jdk curl chromium-browser mysql-server nginx -y
